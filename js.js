e96fa0b2da4ae63c69190dd7cd5f11d42cdc259c
Gwenillia <55385809+Gwenillia@users.noreply.github.com>
2020-11-08 05:37:56 +0100

    rangeage et update

--- js.js
+++ js.js
@@ -1,8 +1,5 @@
 "use strict";
 
-let stats = [];
-let statMax = 26;
-
 const nomStat = [
   "Diplomacie",
   "Martial",
@@ -12,17 +9,28 @@ const nomStat = [
   "Âge",
 ];
 
-while (stats.length < 5) {
-  let j = Math.floor(Math.random() * (statMax - 0) + 0);
-  stats.push(j);
-  statMax -= j;
-}
-const age = stats.reduce((a, b) => a + b, 0);
-stats.push(age);
+function randomize() {
+  const stats = [];
+  let statMax = 26;
 
-for (let i = 0; i < stats.length; i++) {
-  let list = document.createElement("li");
-  list.className = "lol";
-  list.innerHTML = `${nomStat[i]}: ${stats[i]}`;
-  document.getElementById("ul").appendChild(list);
+  // Clear ul
+  document.getElementById("ul").innerHTML = "";
+
+  // Generate Stats
+  while (stats.length < 5) {
+    let j = Math.floor(Math.random() * (statMax - 0) + 0);
+    stats.push(j);
+    statMax -= j;
+  }
+
+  const age = stats.reduce((a, b) => a + b, 0);
+  stats.push(age);
+
+  // li = stats + Append Li
+  for (let i = 0; i < stats.length; i++) {
+    let list = document.createElement("li");
+    list.className = "lol";
+    list.innerHTML = `${nomStat[i]}: ${stats[i]}`;
+    document.getElementById("ul").appendChild(list);
+  }
 }
